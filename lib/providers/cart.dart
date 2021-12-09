3d76336727713177c5dfb7a2cebe770b7ede7e76
gannon darcy <gkdarcy93@gmail.com>
2021-12-08 22:48:25 -0500

    hooked up the initial providers for cart feature

---
+++ lib/providers/cart.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/foundation.dart';
+
+//most of the notes that would be revelent to explain this strucute is in products provider.
+
+//class for individual cart item.
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem(
+      {@required this.id,
+      @required this.title,
+      @required this.quantity,
+      @required this.price});
+}
+
+//class for the cart as a whole.
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+//always do this to avoid needing to directly mess with _items from outside components.
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      //change the quanitity...
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+              id: existingCartItem.id,
+              title: existingCartItem.title,
+              price: existingCartItem.price,
+              quantity: existingCartItem.quantity + 1));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              title: title,
+              quantity: 1,
+              price: price));
+    }
+  }
+}
