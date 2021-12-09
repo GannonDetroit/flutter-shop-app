57c48fd612cdbd5cfe743d4961cc3fa339eb054c
gannon darcy <gkdarcy93@gmail.com>
2021-12-09 16:42:24 -0500

    added expanded info on the orders screen

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 // import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 //using ord as way to avoid a name-class error since OrderItem is used in the provider class and here.
 import '../providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
   OrderItem(this.order);
+
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -14,14 +23,46 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
-            subtitle:
-                Text(DateFormat('MM/dd/yyyy - hh:mm').format(order.dateTime)),
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(
+                DateFormat('MM/dd/yyyy - hh:mm').format(widget.order.dateTime)),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon:
+                  _expanded ? Icon(Icons.expand_less) : Icon(Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          //flutter allows you to add an if-statement intisde of an list (column widget) like this, if true it will show the content, if false it will not.
+          // in height we are saying if expanded is true, then show all the products in that order (needs to be widget.order since this is a stateful widget and i'm in the second class)
+          //and allocate 20 pixels per product with a base height of 10 pixels to make sure we have enough space, but if its more than 100 total pixels it will stop it from being too big at that point.
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min(widget.order.products.length * 20.0 + 10, 100),
+              child: ListView(
+                children: widget.order.products
+                    .map((product) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              product.title,
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '${product.quantity}x \$${product.price}',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.grey),
+                            )
+                          ],
+                        ))
+                    .toList(),
+              ),
+            )
         ],
       ),
     );
