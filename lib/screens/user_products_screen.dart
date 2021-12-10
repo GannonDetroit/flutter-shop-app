1a0eb4dae4f7002986d1ac8d1e16d1cf25fccd92
gannon darcy <gkdarcy93@gmail.com>
2021-12-10 14:55:28 -0500

    added the screen and UI for users to upload, edit, and delete their product options

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../widgets/app_drawer.dart';
+import '../providers/products.dart';
+import '../widgets/user_products_item.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Products'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                //nav to new products screen.
+              },
+              icon: Icon(Icons.add))
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemBuilder: (_, index) {
+            return Column(
+              children: [
+                UserProductItem(productsData.items[index].title,
+                    productsData.items[index].imageUrl),
+                Divider()
+              ],
+            );
+          },
+          itemCount: productsData.items.length,
+        ),
+      ),
+    );
+  }
+}
