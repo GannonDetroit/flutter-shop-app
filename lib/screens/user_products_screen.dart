9eb5b0cc5737246cf350099a11b644dc2662b63a
gannon darcy <gkdarcy93@gmail.com>
2021-12-14 11:49:57 -0500

    added pull down to refresh feature

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -7,6 +7,13 @@ import '../widgets/user_products_item.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
+
+//since this is not a stateful widget, you need to add context as a arg for it work via BuildContext;
+//remember that async await always returns a future automatically so it makes it easier to use on things that expect futures to be returned, like the onRefresh method.
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -23,21 +30,25 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemBuilder: (_, index) {
-            return Column(
-              children: [
-                UserProductItem(
-                    productsData.items[index].id,
-                    productsData.items[index].title,
-                    productsData.items[index].imageUrl),
-                Divider()
-              ],
-            );
-          },
-          itemCount: productsData.items.length,
+      //the RefreshIndicator gives us pull to refresh functionality out of the box, with a loading spinner and everything.
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemBuilder: (_, index) {
+              return Column(
+                children: [
+                  UserProductItem(
+                      productsData.items[index].id,
+                      productsData.items[index].title,
+                      productsData.items[index].imageUrl),
+                  Divider()
+                ],
+              );
+            },
+            itemCount: productsData.items.length,
+          ),
         ),
       ),
     );
