ddc949ab5a199f79a67873c5cb73339685ce2c51
gannon darcy <gkdarcy93@gmail.com>
2021-12-26 20:19:42 -0500

    added animation to orders drop down effect

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -18,31 +18,38 @@ class _OrderItemState extends State<OrderItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('\$${widget.order.amount}'),
-            subtitle: Text(
-                DateFormat('MM/dd/yyyy - hh:mm').format(widget.order.dateTime)),
-            trailing: IconButton(
-              icon:
-                  _expanded ? Icon(Icons.expand_less) : Icon(Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height:
+          _expanded ? min(widget.order.products.length * 20.0 + 110, 200) : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('\$${widget.order.amount}'),
+              subtitle: Text(DateFormat('MM/dd/yyyy - hh:mm')
+                  .format(widget.order.dateTime)),
+              trailing: IconButton(
+                icon: _expanded
+                    ? Icon(Icons.expand_less)
+                    : Icon(Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-          ),
-          //flutter allows you to add an if-statement intisde of an list (column widget) like this, if true it will show the content, if false it will not.
-          // in height we are saying if expanded is true, then show all the products in that order (needs to be widget.order since this is a stateful widget and i'm in the second class)
-          //and allocate 20 pixels per product with a base height of 10 pixels to make sure we have enough space, but if its more than 100 total pixels it will stop it from being too big at that point.
-          if (_expanded)
-            Container(
+            //flutter allows you to add an if-statement intisde of an list (column widget) like this, if true it will show the content, if false it will not.
+            // in height we are saying if expanded is true, then show all the products in that order (needs to be widget.order since this is a stateful widget and i'm in the second class)
+            //and allocate 20 pixels per product with a base height of 10 pixels to make sure we have enough space, but if its more than 100 total pixels it will stop it from being too big at that point.
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
               padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20.0 + 10, 100),
+              height: _expanded
+                  ? min(widget.order.products.length * 20.0 + 10, 100)
+                  : 0,
               child: ListView(
                 children: widget.order.products
                     .map((product) => Row(
@@ -63,7 +70,8 @@ class _OrderItemState extends State<OrderItem> {
                     .toList(),
               ),
             )
-        ],
+          ],
+        ),
       ),
     );
   }
